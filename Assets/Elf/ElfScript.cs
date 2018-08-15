9f3a0e12c2070935846c380caa57cc1cef5af7c2
Daniel <fighteronfire@gmx.net>
2018-08-15 23:57:20 +0200

    Add empty Projekt

---
+++ Assets/Elf/ElfScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElfScript : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey (KeyCode.W))
+        {
+            anim.Play("walking");
+        }
+       else
+        {
+           
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.Play("running");
+        }
+        else
+        {
+            
+        }
+    }
+}
---
+++ Assets/Island/Scripts/CharacterMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W)){
+            anim.Play("Walking Cycle");
+        }
+        if (Input.GetKeyDown(KeyCode.W) && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.Play("Run Cycle");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.Play("Jump Cycle");
+        }
+
+
+
+    }
+}
+
