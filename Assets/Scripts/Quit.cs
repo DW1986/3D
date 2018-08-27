1d32a67f0f65fe2a6d633170017918d705aa7032
Daniel <fighteronfire@gmx.net>
2018-08-27 20:29:45 +0200

    Quit Button + Scene change
    Quit Button im CharAuswahl
    Scene Wechsel in Scene 1 mit ESC Taste

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+    public void onQuit()
+    {
+
+        Debug.Log("Quit");
+        Application.Quit();
+        
+    }
+}
